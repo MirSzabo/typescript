25a58663af095a02afdd96193b77849cf372b584
MirSzabo <48127476+MirSzabo@users.noreply.github.com>
2020-04-29 23:56:09 +0200

    bmi calculator

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,5 +1,13 @@
-const calculateBmi  = (weight: number , height: number) => {
-    return (weight  / height);
+const calculateBmi = (weight: number, height: number) => {
+  const bmi = weight / height;//kg/m2
+  if (bmi < 18.5) {
+    return "Underweight";
+  } else if (bmi > 18.5 && bmi < 25) {
+    return "Normal (healthy weight)";
+  } else if (bmi > 25 && bmi < 30) {
+    return "Overweight ";
+  } else if (bmi > 30) {
+    return "Obese ";
   }
-  console.log(calculateBmi(180, 74))
- 
+};
+console.log(calculateBmi(180, 74));
