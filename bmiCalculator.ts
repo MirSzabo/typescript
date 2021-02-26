826d55164de6c747f01c55a2d465abda57a42ffc
MirSzabo <poradova.m@gmail.com>
2021-02-26 23:43:29 +0100

    9.1 exercise

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,13 +1,13 @@
-const calculateBmi = (weight: number, height: number) => {
-  const bmi = weight / height;//kg/m2
+const calculateBmi = (height: number, weight: number) => {
+  const bmi = weight / Math.pow((height/100),2);//kg/m2
   if (bmi < 18.5) {
     return "Underweight";
-  } else if (bmi > 18.5 && bmi < 25) {
-    return "Normal (healthy weight)";
-  } else if (bmi > 25 && bmi < 30) {
-    return "Overweight ";
+  } else if (bmi > 18.5 && bmi < 24.9) {
+    return "Normal weight";
+  } else if (bmi > 25 && bmi < 29.9) {
+    return "Overweight";
   } else if (bmi > 30) {
-    return "Obese ";
+    return "Obesity";
   }
 };
 console.log(calculateBmi(180, 74));
